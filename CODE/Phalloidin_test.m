f0c16b17376458ecf425f6b80ec1294501253c50
youssefarafat <youssefarafat25@gmail.com>
2022-04-21 10:51:44 +0100

    Update Phalloidin_test.m

--- CODE/Phalloidin_test.m
+++ CODE/Phalloidin_test.m
@@ -3,3 +3,10 @@ red_channel = img2(:,:,1);
 red_channel_filt       = imfilter(red_channel,fspecial('Gaussian',5));
 red_channel_thres      = red_channel_filt>120;
 red_channel_labelled   = bwlabel(red_channel_thres);
+img3 = imread("RBD_LKR13_1_Phalloidin.tif");
+red_channel_filled     = imfill(red_channel_labelled);
+red_channel_clean = bwareaopen(red_channel_filled, 150);
+red_channel_closed = imclose(red_channel_clean, strel('disk',30));
+red_channel_dilate = imdilate(red_channel_closed, strel('disk', 10));
+imagesc(red_channel_clean + red_channel_dilate)
+
