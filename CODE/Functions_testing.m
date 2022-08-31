f9e6d1bbb0b133a40939fe03eaf5744028aa4591
youssefarafat <youssefarafat25@gmail.com>
2022-08-31 16:32:27 +0100

    Create Functions_testing.m

---
+++ CODE/Functions_testing.m
@@ -0,0 +1,7 @@
+dataIn = imread('RBD_LKR13_1_Phalloidin.tif');
+function dataOut = PhalloidinRBD(dataIn);
+dataOut.red_channelR_distance = red_channelR_distance; 
+dataOut.red_channelR_skel = red_channelR_skel;
+dataOut.red_channelR_split = red_channelR_split;
+end
+
