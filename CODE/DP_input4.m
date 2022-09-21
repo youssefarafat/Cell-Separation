7e8c2bdf18a7cd3a2acd447018047d3b419cb339
youssefarafat <youssefarafat25@gmail.com>
2022-09-21 20:33:43 +0100

    Create DP_input4.m

---
+++ CODE/DP_input4.m
@@ -0,0 +1,32 @@
+%% OUTPUT DAPI PHALLOIDIN COMBINATION PLOTS
+figure
+
+h1 = subplot(2,5,1)
+imagesc(imread("IDP_R_1.jpg"))
+
+h2 = subplot(2,5,2)
+imagesc(imread("IDP_R_1_1.png"))
+
+h3 = subplot(2,5,3)
+imagesc(imread("IDP_R_1_2.png"))
+
+h4 = subplot(2,5,4)
+imagesc(imread("IDP_R_1_4.png"))
+
+h5 = subplot(2,5,5)
+imagesc(imread("IDP_R_1_5.png"))
+
+h6 = subplot(2,5,6)
+imagesc(imread("IDP_W_1.png"))
+
+h7 = subplot(2,5,7)
+imagesc(imread("IDP_W_1_1.png"))
+
+h8 = subplot(2,5,8)
+imagesc(imread("IDP_W_1_2.png"))
+
+h9 = subplot(2,5,9)
+imagesc(imread("IDP_W_1_3.png"))
+
+h10 = subplot(2,5,10)
+imagesc(imread("IDP_W_1_6.png"))
