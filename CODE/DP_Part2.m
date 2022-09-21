9dde813ae43d5b636eb4a51f066e418892405861
youssefarafat <youssefarafat25@gmail.com>
2022-09-21 20:34:08 +0100

    Create DP_Part2.m

---
+++ CODE/DP_Part2.m
@@ -0,0 +1,4 @@
+ %% DAPI PHALLOIDIN COMBINATION MEAN UNIQUE CELLS ISLAND BARPLOT
+   unique_findings = [fmean_unique_cells_island1,fmean_unique_cells_island2,fmean_unique_cells_island3,fmean_unique_cells_island4,fmean_unique_cells_island5,fmean_unique_cells_island6,fmean_unique_cells_island7,fmean_unique_cells_island8,fmean_unique_cells_island9,fmean_unique_cells_island10];
+   figure
+   bar(unique_findings)
