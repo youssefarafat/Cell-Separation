ce4811c000aa1b1af7c5aa9e84e6515f25537ac7
youssefarafat <youssefarafat25@gmail.com>
2022-09-21 20:33:56 +0100

    Create DP_Part1.m

---
+++ CODE/DP_Part1.m
@@ -0,0 +1,4 @@
+%% DAPI PHALLOIDIN COMBINATION MEAN CELL AREA RATIO BARPLOT
+ratio_findings = [fmean_ratio1,fmean_ratio2,fmean_ratio3,fmean_ratio4,fmean_ratio5,fmean_ratio6,fmean_ratio7,fmean_ratio8,fmean_ratio9,fmean_ratio10];
+figure
+bar(ratio_findings)
