57d8775a4134fc59a99a61894d80ee9ffe11c705
youssefarafat <youssefarafat25@gmail.com>
2022-09-27 20:43:35 +0100

    Update DAPI_All_Final_Cells.m

--- CODE/DAPI_All_Final_Cells.m
+++ CODE/DAPI_All_Final_Cells.m
@@ -6,6 +6,8 @@ disp(k)
 dataIn = imread(dir0(k).name);
 dataOut = cellseperationtest(dataIn);
 h1(k) = subplot(2,5,k)
-h2(k) = imagesc(dataOut.overlaid)
+imagesc(dataOut.overlaid)
 title(num2str(k))
+ylabel = ylabel(dir0(k).name,'interpreter','none')
+
 end
