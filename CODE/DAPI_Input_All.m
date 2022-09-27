54fa0e6761f11c55013ebb97b2a1d788d5e4219b
youssefarafat <youssefarafat25@gmail.com>
2022-09-27 20:43:55 +0100

    Update DAPI_Input_All.m

--- CODE/DAPI_Input_All.m
+++ CODE/DAPI_Input_All.m
@@ -5,4 +5,19 @@ for k = 1:10
 h(k) = subplot(2,5,k)
 imagesc(imread(dir0(k).name));
 title(num2str(k))
+ylabel(dir0(k).name,'interpreter','none')
 end
+
+for k = 1:5
+    h(k).Position = [-0.14+k*0.195 0.55 0.145 0.42];
+    h(k).YLabel.FontSize=11;
+    h(k).Title.FontSize=9;
+
+end 
+
+for k = 6:10
+    h(k).Position = [-0.14+(k-5)*0.195 0.05 0.145 0.42];
+    h(k).YLabel.FontSize=11;
+    h(k).Title.FontSize=9;
+
+end 
