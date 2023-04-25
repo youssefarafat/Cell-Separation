087cb3c45a1b1920ed3ff3a140a72db8ec1178fc
youssefarafat <youssefarafat25@gmail.com>
2023-04-25 23:36:09 +0100

    Update Phalloidin_all_final.m

--- CODE/Phalloidin_all_final.m
+++ CODE/Phalloidin_all_final.m
@@ -5,10 +5,26 @@ dir0 = dir('*Phalloidin.tiff');
     for k = 1:10
         disp(k)
         dataIn1 = imread(dir0(k).name);
-        dataOut = PhalloidinRBD(dataIn1);
+        dataOut2 = PhalloidinRBD(dataIn1);
         h1(k) = subplot(2,5,k);
-        h2(k) = imagesc(dataOut.labeled_filt_islands);
+        h2(k) = imagesc(dataOut2.labeled_filt_islands);
         %h2(k) = imagesc(dataIn1);
         %h3(k) = gca;
-        title(num2str(k))
+        title(strcat('Number of Islands=',32,num2str(dataOut2.n_islands,2)))
+        ylabel(dir0(k).name,'interpreter','none')
     end
+    
+
+    for k = 1:5
+    h1(k).Position = [-0.14+k*0.195 0.55 0.145 0.42];
+    h1(k).YLabel.FontSize=11;
+    h1(k).Title.FontSize=9;
+
+end 
+
+for k = 6:10
+    h1(k).Position = [-0.14+(k-5)*0.195 0.05 0.145 0.42];
+    h1(k).YLabel.FontSize=11;
+    h1(k).Title.FontSize=9;
+
+end 
