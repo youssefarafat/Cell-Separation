04f379717c6f19f8a3e096a6fb1ebbfa5f79d603
youssefarafat <youssefarafat25@gmail.com>
2023-04-25 23:59:52 +0100

    Update Phalloidin_Part3.m

--- CODE/Phalloidin_Part3.m
+++ CODE/Phalloidin_Part3.m
@@ -1,62 +1,71 @@
 %% PHALLOIDIN METHOD PART 3
 figure
 
-subplot(2,6,1)
-h(1) = imagesc((red_channelR_distance))
-h(1) = gca;
 
-subplot(2,6,2)
-h(2) = imagesc((red_channelR_dilate))
-h(2) = gca;
 
-subplot(2,6,3)
-h(3) = imagesc(red_channelR_skel)
-h(3) = gca;
+subplot(2,5,1)
+h1 = imagesc(red_channelR_skel)
+h1 = gca;
 
-subplot(2,6,4)
-h(4) = imagesc(step4)
-h(4) = gca;
+subplot(2,5,2)
+h2 = imagesc(step4)
+h2 = gca;
 
-subplot(2,6,5)
-h(5) = imagesc(red_channelR_dilatesplit)
-h(5) = gca;
+subplot(2,5,3)
+h3 = imagesc(red_channelR_dilatesplit)
+h3 = gca;
 
-subplot(2,6,6)
-h(6) = imagesc(red_channelR_split)
-h(6) = gca;
+subplot(2,5,4)
+h4 = imagesc(red_channelR_split)
+h4 = gca;
 
-subplot(2,6,7)
-h(7) = imagesc(red_channelR_distance)
-h(7) = gca;
-h(7).XLim=[700 950];
-h(7).YLim=[200 400];
+subplot(2,5,5)
+h5 = imagesc(edgedetect)
+h5 = gca;
 
-subplot(2,6,8)
-h(8) = imagesc(red_channelR_dilate)
-h(8) = gca;
-h(8).XLim=[700 950];
-h(8).YLim=[200 400];
+subplot(2,5,6)
+h6 = imagesc(red_channelR_skel)
+h6 = gca;
+h6.XLim=[700 950];
+h6.YLim=[200 400];
 
-subplot(2,6,9)
-h(9) = imagesc(red_channelR_skel)
-h(9) = gca;
-h(9).XLim=[700 950];
-h(9).YLim=[200 400];
+subplot(2,5,7)
+h7 = imagesc(step4)
+h7 = gca;
+h7.XLim=[700 950];
+h7.YLim=[200 400];
 
-subplot(2,6,10)
-h(10) = imagesc(step4)
-h(10) = gca;
-h(10).XLim=[700 950];
-h(10).YLim=[200 400];
+subplot(2,5,8)
+h8 = imagesc(red_channelR_dilatesplit)
+h8 = gca;
+h8.XLim= [700 950];
+h8.YLim= [200 400];
 
-subplot(2,6,11)
-h(11) = imagesc(red_channelR_dilatesplit)
-h(11) = gca;
-h(11).XLim= [700 950];
-h(11).YLim= [200 400];
+subplot(2,5,9)
+h9 = imagesc(red_channelR_split)
+h9 = gca;
+h9.XLim= [700 950];
+h9.YLim= [200 400];
 
-subplot(2,6,12)
-h(12) = imagesc(red_channelR_split)
-h(12) = gca;
-h(12).XLim= [700 950];
-h(12).YLim= [200 400];
+subplot(2,5,10)
+h10 = imagesc(edgedetect)
+h10 = gca;
+h10.XLim= [700 950];
+h10.YLim= [200 400];
+
+for k = 1:5
+    h(k).Position = [-0.14+k*0.195 0.55 0.145 0.42];
+    h(k).YLabel.FontSize=11;
+    h(k).Title.FontSize=9;
+
+end 
+
+for k = 6:10
+    h(k).Position = [-0.14+(k-5)*0.195 0.05 0.145 0.42];
+    h(k).YLabel.FontSize=11;
+    h(k).Title.FontSize=9;
+
+end 
+
+filename='Phalloidin_segmentation_3.png';
+print('-dpng','-r200',filename)
