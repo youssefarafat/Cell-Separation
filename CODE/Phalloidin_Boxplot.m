85f38876089775405f6aaa6573079c04868b065a
youssefarafat <youssefarafat25@gmail.com>
2023-04-25 23:36:50 +0100

    Update Phalloidin_Boxplot.m

--- CODE/Phalloidin_Boxplot.m
+++ CODE/Phalloidin_Boxplot.m
@@ -1,11 +1,36 @@
 %% PHALLOIDIN MEAN AREA ISLANDS P-VALUE & BOXPLOT
 figure
+clear results*
+results2=[];
+results3=[];
+results4=[];
+
 dir0 = dir('*Phalloidin.tiff');
 for k = 1:10
-dataIn1 = imread(dir0(k).name);
-dataOut = PhalloidinRBD(dataIn1);
-results(k) = dataOut.mean_area_islands
+
+    dataIn1 = imread(dir0(k).name);
+    dataOut2 = PhalloidinRBD(dataIn1);
+    %results(k) = dataOut2.mean_area_islands;
+    %results(k) = dataOut2.ftotal_area;
+    %results(k) = dataOut2.total_area;
+    results2=[results2 ; dataOut2.total_area'];
+    results3=[results3 ; k*ones(numel(dataOut2.total_area),1)];
+
+        
+    for y = 1:(dataOut2.n_islands)
+        %results(k)  = dataOut2.area_results(y);
+        results(k,y) = dataOut2.total_area(y);
+        %results2=[results2 ; ]
+        %results(k) = dataOut2.area_results;
+    end
 end
-results1 = rand(10,2)
-[t,p,r] = ttest2(results([1:5]),results([6:10]))
-boxplot(results1)
+%%
+[t,p,r] = ttest2(results2([1:19]),results2([20:29]));
+%boxplot(results,[1 1 1 1 1 2 2 2 2 2])
+(1:10)>5
+ boxplot(results2,results3>5,'Labels',{'RBD','WT'})
+ title(strcat('p=',32,num2str(p,2)))
+ filename='Phalloidin_Area_boxplot.png';
+print('-dpng','-r200',filename)
+
+ bar(results2,results3>5)
